b06d813beae7cbee96cb14be9a7a6e2cb6b4a99f
Rylie Castell <rylie.castell@gmail.com>
2023-10-24 11:50:12 +0200

    add app header

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
 import {useNavigate} from "react-router-dom";
+import styled from "styled-components";
+import AppHeader from "../components/AppHeader.tsx";
 
 export default function HomePage() {
     const navigateTo = useNavigate();
@@ -12,6 +14,7 @@ export default function HomePage() {
     }
 
     return <>
+        <AppHeader headerText="Home"/>
         <div>This is the HomePage.</div>
         <button type="button" onClick={handleClickAllTransactions}>See all transactions</button>
         <button type="button" onClick={handleClickNewTransaction}>Add new transaction</button>
