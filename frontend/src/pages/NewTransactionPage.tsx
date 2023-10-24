4c16165f03461f11594a9820dd67e83f2b701104
Rylie Castell <rylie.castell@gmail.com>
2023-10-24 13:30:54 +0200

    edit submit button

--- frontend/src/pages/NewTransactionPage.tsx
+++ frontend/src/pages/NewTransactionPage.tsx
@@ -19,6 +19,61 @@ const Form = styled.form`
   flex-direction: column;
   gap: 0.6em;
 `;
+
+const SubmitButton = styled.button`align-items: center;
+  background-color: #d6c7c7;
+  border: 0.2em solid #111;
+  border-radius: 0.4em;
+  box-sizing: border-box;
+  color: #111;
+  cursor: pointer;
+  display: flex;
+  font-family: 'Inter', sans-serif;
+  font-size: 16px;
+  height: 48px;
+  justify-content: center;
+  line-height: 24px;
+  max-width: 100%;
+  padding: 1.4em;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+
+  &:after {
+    background-color: #111;
+    border-radius: 8px;
+    content: "";
+    display: block;
+    height: 48px;
+    left: 0;
+    width: 100%;
+    position: absolute;
+    top: -2px;
+    transform: translate(8px, 8px);
+    transition: transform 0.2s ease-out;
+    z-index: -1;
+  }
+
+  &:hover:after {
+    transform: translate(0, 0);
+  }
+
+  &:active {
+    background-color: #ddadad;
+    outline: 0;
+  }
+
+  &:hover {
+    outline: 0;
+  }
+
+  @media (min-width: 768px) {
+    padding: 0 40px;
+  }
+`;
 export default function NewTransactionPage() {
     const navigateTo = useNavigate();
     function handleSubmitForm(event: React.FormEvent) {
@@ -55,7 +110,7 @@ export default function NewTransactionPage() {
             <input name={"title"} id={"title"} type={"text"} required/>
             <label htmlFor={"moneyAmount"}>Amount of Money:</label>
             <input name={"amountOfMoney"} id={"moneyAmount"} type={"text"} required/>
-            <Button buttonType={"submit"} buttonText="Submit" handleButtonClick={handleSubmitForm}/>
+            <SubmitButton type="submit">Submit</SubmitButton>
         </Form>
         </Main>
     </>
