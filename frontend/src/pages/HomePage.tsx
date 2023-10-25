f6204098b99b467aee7b317b56624a29e625a438
Rylie Castell <rylie.castell@gmail.com>
2023-10-25 14:46:15 +0200

    add me button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import {useNavigate} from "react-router-dom";
 import styled from "styled-components";
 import AppHeader from "../components/AppHeader.tsx";
 import Button from "../components/Button.tsx";
+import axios from "axios";
 
 const Main = styled.main`
   display: flex;
@@ -16,6 +17,7 @@ const Main = styled.main`
 const ButtonContainer = styled.div`
   display: flex;
   gap: 0.6em;
+  justify-content: center;
 `;
 
 export default function HomePage() {
@@ -30,20 +32,30 @@ export default function HomePage() {
     }
 
     function handleClickLogin() {
-        const host : string = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin;
+        const host: string = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin;
         window.open(host + '/oauth2/authorization/github', '_blank');
+    }
+
+    function handleClickMe() {
+        axios.get("/api/users/me")
+            .then(response => {
+                console.log("Username: " + response.data);
+            })
     }
 
     return <>
         <AppHeader headerText="Home"/>
         <Main>
-        <div>This is the HomePage.</div>
-            <Button buttonText="Login" onClick={handleClickLogin}/>
-        <ButtonContainer>
-            <Button onClick={handleClickAllTransactions}
-                    buttonText="See all transactions"/>
-            <Button onClick={handleClickNewTransaction} buttonText="Add new transaction"/>
-        </ButtonContainer>
+            <div>This is the HomePage.</div>
+            <ButtonContainer>
+                <Button buttonText="Login with Github" onClick={handleClickLogin}/>
+                <Button buttonText="Get my name" onClick={handleClickMe}/>
+            </ButtonContainer>
+            <ButtonContainer>
+                <Button onClick={handleClickAllTransactions}
+                        buttonText="All transactions"/>
+                <Button onClick={handleClickNewTransaction} buttonText="New transaction"/>
+            </ButtonContainer>
         </Main>
     </>
 }
