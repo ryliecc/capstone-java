8a2253a9d5a4a5ad41516ed28c2e627be4934580
Rylie Castell <rylie.castell@gmail.com>
2023-10-25 13:45:01 +0200

    add oauth configuration

---
+++ backend/src/main/java/com/github/ryliecc/backend/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.github.ryliecc.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll())
+                .sessionManagement(sessions ->
+                        sessions.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .oauth2Login(withDefaults());
+        return httpSecurity.build();
+    }
+}
