c0318853c6f91fb04402e7148dc1b98c04292409
Rylie Castell <rylie.castell@gmail.com>
2023-10-28 15:38:12 +0200

    fix unused import

--- backend/src/main/java/com/github/ryliecc/backend/service/BudgetService.java
+++ backend/src/main/java/com/github/ryliecc/backend/service/BudgetService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
