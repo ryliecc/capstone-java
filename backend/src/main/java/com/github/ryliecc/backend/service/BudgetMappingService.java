d6bfef5be21fda9dd098f422b6af8720e77afc2b
Rylie Castell <rylie.castell@gmail.com>
2023-10-28 15:34:10 +0200

    add tests for budget mapping service

--- backend/src/main/java/com/github/ryliecc/backend/service/BudgetMappingService.java
+++ backend/src/main/java/com/github/ryliecc/backend/service/BudgetMappingService.java
@@ -45,4 +45,5 @@ public class BudgetMappingService {
                 .categoryType(newCategory.getCategoryType())
                 .build();
     }
+
 }
--- backend/src/test/java/service/BudgetMappingServiceTest.java
+++ backend/src/test/java/service/BudgetMappingServiceTest.java
@@ -1,8 +1,6 @@
 package service;
 
-import com.github.ryliecc.backend.models.NewTransaction;
-import com.github.ryliecc.backend.models.TransactionEntry;
-import com.github.ryliecc.backend.models.TransactionsResponse;
+import com.github.ryliecc.backend.models.*;
 import com.github.ryliecc.backend.service.BudgetMappingService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -50,5 +48,41 @@ class BudgetMappingServiceTest {
         assert actual.getAmountOfMoney().equals("4.20");
         assert actual.getTimeLogged() != null;
         assert actual.getCreatorId().equals("testId");
+    }
+
+    @Test
+    void mapCategoryToResponse() {
+        // GIVEN
+        TransactionCategory transactionCategory = new TransactionCategory();
+        transactionCategory.setId("category_id");
+        transactionCategory.setTitle("category_title");
+        transactionCategory.setCreatorId("category_creator_id");
+        transactionCategory.setCategoryType("category_type");
+
+        // WHEN
+        CategoryResponse actual = budgetMappingService.mapCategoryToResponse(transactionCategory);
+
+        // THEN
+        Assertions.assertEquals("category_id", actual.id());
+        assert actual.title().equals("category_title");
+        assert actual.creatorId().equals("category_creator_id");
+        assert actual.categoryType().equals("category_type");
+    }
+
+    @Test
+    void mapNewCategoryToTransactionCategory() {
+        // GIVEN
+        NewCategory newCategory = new NewCategory();
+        newCategory.setTitle("new_category_title");
+        newCategory.setCreatorId("new_category_creator_id");
+        newCategory.setCategoryType("new_category_type");
+
+        // WHEN
+        TransactionCategory actual = budgetMappingService.mapNewCategoryToTransactionCategory(newCategory);
+
+        // THEN
+        Assertions.assertEquals("new_category_title", actual.getTitle());
+        assert actual.getCreatorId().equals("new_category_creator_id");
+        assert actual.getCategoryType().equals("new_category_type");
     }
 }
