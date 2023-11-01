f10f98f34e0bff372d8f7d468fa32d67a11ad0a8
Rylie Castell <rylie.castell@gmail.com>
2023-11-01 18:12:33 +0100

    fix security hotspot

--- frontend/src/hooks/formatMoney.tsx
+++ frontend/src/hooks/formatMoney.tsx
@@ -3,7 +3,20 @@ export default function formatMoney(input: string): string {
     const numericString = input.replace(/[^0-9.]/g, '');
     const [dollars, cents] = numericString.split('.');
     const formattedCents = cents ? cents.padEnd(2, '0') : '00';
-    const formattedDollars = dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    // Add commas as thousands separators
+    const formattedDollars = addThousandsSeparator(dollars);
+
     const formattedMoney = `${formattedDollars}.${formattedCents}`;
     return isNegative ? `-${formattedMoney}` : formattedMoney;
+}
+
+function addThousandsSeparator(input: string): string {
+    const parts = input.split('.');
+    const integerPart = parts[0];
+    const fractionalPart = parts[1] || '';
+    const reversedIntegerPart = integerPart.split('').reverse().join('');
+    const formattedIntegerPart = reversedIntegerPart.replace(/\d{3}(?=\d)/g, (match) => `${match},`);
+    const finalFormattedIntegerPart = formattedIntegerPart.split('').reverse().join('');
+    return finalFormattedIntegerPart + (fractionalPart ? `.${fractionalPart}` : '');
 }
