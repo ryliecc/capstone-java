7cecfded559ccd75f413ee2eb2b031c7a012d5a8
Rylie Castell <rylie.castell@gmail.com>
2023-11-03 21:59:04 +0100

    edit delete button

--- frontend/src/components/TransactionDetailsWindow.tsx
+++ frontend/src/components/TransactionDetailsWindow.tsx
@@ -5,19 +5,23 @@ import CloseIcon from "../assets/x-mark.svg";
 import Button from "./Button.tsx";
 import formatMoney from "../hooks/formatMoney.tsx";
 import DeleteIcon from "../assets/trash.svg";
+import axios from "axios";
+import DeleteRecurringWindow from "./DeleteRecurringWindow.tsx";
 
 export type props = {
     transaction: Transaction,
     isVisible: boolean,
     // eslint-disable-next-line @typescript-eslint/ban-types
-    setIsVisible: Function
+    setIsVisible: Function,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    setTransactions: Function
 }
 
-const Container = styled.div<{isVisible: boolean}>`
+const Container = styled.div<{ $isVisible: boolean }>`
   position: fixed;
   height: 100%;
   width: 100%;
-  top: ${(props) => (props.isVisible ? "0" : "-100%")};
+  top: ${(props) => (props.$isVisible ? "0" : "-100%")};
   left: 0;
   z-index: 3;
   background-color: whitesmoke;
@@ -28,11 +32,11 @@ const Container = styled.div<{isVisible: boolean}>`
 const CloseButton = styled.button``;
 
 const CloseImage = styled.img`
-    width: 3em;
+  width: 3em;
 `;
 
 const EditButton = styled.button`
-    color: black;
+  color: black;
 `;
 
 const Title = styled.h2`
@@ -41,38 +45,47 @@ const Title = styled.h2`
 `;
 
 const MoneyAmount = styled.p`
-    color: black;
+  color: black;
 `;
 
 const Category = styled.div`
-    color: black;
+  color: black;
 `;
 
 const TimeLabel = styled.p`
-    color: black;
+  color: black;
 `;
 
 const RecurringLabel = styled.div`
-    color: black;
+  color: black;
 `;
 
 const DeleteButton = styled.button`
-    color: black;
+  color: black;
 `;
 
 const DeleteImage = styled.img`
-    width: 3em;
+  width: 3em;
 `;
 
 const EditForm = styled.form``;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 0.8em;
+`;
 export default function TransactionDetailsWindow(props: Readonly<props>) {
     const [isEditForm, setIsEditForm] = useState(false);
     const isRecurring: boolean = props.transaction.referenceId !== "daily_transaction";
+    const [isDeleteWindowVisible, setIsDeleteWindowVisible] = useState(false);
 
-    const TimeElement = isRecurring ? (<RecurringLabel>Recurring Element</RecurringLabel>) : (<TimeLabel>Date: {props.transaction.timeLogged}</TimeLabel>);
+    const TimeElement = isRecurring ? (<RecurringLabel>Recurring Element</RecurringLabel>) : (
+        <TimeLabel>Date: {props.transaction.timeLogged}</TimeLabel>);
 
     function handleClickCloseWindow() {
         props.setIsVisible(false);
+        setIsEditForm(false);
     }
 
     function handleClickEditButton() {
@@ -80,7 +93,21 @@ export default function TransactionDetailsWindow(props: Readonly<props>) {
     }
 
     function handleClickDeleteButton() {
-        console.log("Delete was clicked.")
+        if (props.transaction.referenceId === "daily_transaction") {
+            axios
+                .delete("/api/budget-app/" + props.transaction.id)
+                .then(() => {
+                    props.setTransactions((prevTransactions: Transaction[]) => {
+                        return prevTransactions.filter((transaction: Transaction) => transaction.id !== props.transaction.id);
+                    });
+                    props.setIsVisible(false);
+                })
+                .catch((error) => {
+                    console.error("Fehler beim Löschen", error);
+                });
+        } else {
+            setIsDeleteWindowVisible(true);
+        }
     }
 
     function handleSubmitEditForm(event: React.FormEvent) {
@@ -88,14 +115,26 @@ export default function TransactionDetailsWindow(props: Readonly<props>) {
         console.log("Submit was clicked.")
     }
 
-    if(isEditForm) {
-        return <Container isVisible={props.isVisible}>
+    function handleClickCancelEdit() {
+        setIsEditForm(false);
+    }
+
+    if (isEditForm) {
+        return <Container $isVisible={props.isVisible}>
+            <CloseButton type="button" onClick={handleClickCloseWindow}><CloseImage src={CloseIcon}
+                                                                                    alt="Close Button"/></CloseButton>
             <EditForm onSubmit={handleSubmitEditForm}>
-                <Button buttonText="Submit" type="submit"/>
+                <ButtonContainer>
+                    <Button buttonText="Submit" type="submit"/>
+                    <Button buttonText="Cancel" onClick={handleClickCancelEdit} />
+                </ButtonContainer>
             </EditForm>
         </Container>
     }
-    return <Container isVisible={props.isVisible}>
+    return <Container $isVisible={props.isVisible}>
+        <DeleteRecurringWindow setIsVisible={setIsDeleteWindowVisible} isVisible={isDeleteWindowVisible}
+                               id={props.transaction.id} referenceId={props.transaction.referenceId}
+                               setTransactions={props.setTransactions}/>
         <CloseButton type="button" onClick={handleClickCloseWindow}><CloseImage src={CloseIcon}
                                                                                 alt="Close Button"/></CloseButton>
         <EditButton type="button" onClick={handleClickEditButton}>Edit</EditButton>
--- frontend/src/pages/AllTransactionsPage.tsx
+++ frontend/src/pages/AllTransactionsPage.tsx
@@ -3,10 +3,8 @@ import axios from "axios";
 import {Transaction} from "../models/TransactionModel.tsx";
 import AppHeader from "../components/AppHeader.tsx";
 import styled from "styled-components";
-import TrashIcon from "../assets/trash.svg";
 import Background from "../components/Background.tsx";
 import BackButton from "../components/BackButton.tsx";
-import DeleteRecurringWindow from "../components/DeleteRecurringWindow.tsx";
 import formatMoney from "../hooks/formatMoney.tsx";
 import AppMenu from "../components/AppMenu.tsx";
 import {Main} from "../components/Main.tsx";
@@ -34,7 +32,6 @@ const ListItem = styled.button`
 const DataContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  padding-right: 3.2em;
 `;
 
 const Category = styled.div`
@@ -46,32 +43,8 @@ const Category = styled.div`
   border-radius: 5px;
 `;
 
-const DeleteButton = styled.button`
-  border-radius: 10px;
-  position: absolute;
-  align-self: end;
-  padding: 0;
-  background-color: #aec8ce;
-  border: none;
-  cursor: pointer;
-  height: 2.6em;
-  width: 2.6em;
-  font-size: 1em;
-  top: 0.6em;
-`;
-
-const ButtonImage = styled.img`
-  position: absolute;
-  left: 0.3em;
-  top: 0.3em;
-  width: 2em;
-`;
-
 export default function AllTransactionsPage() {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
-    const [isDeleteWindowVisible, setIsDeleteWindowVisible] = useState(false);
-    const [deleteId, setDeleteId] = useState("");
-    const [deleteReferenceId, setDeleteReferenceId] = useState("");
     const [creatorId] = useLocalStorageState("creatorId", {defaultValue: "anonymousUser"});
     const initialTransaction: Transaction = {
         id: "",
@@ -93,28 +66,6 @@ export default function AllTransactionsPage() {
                 setTransactions(response.data);
             });
     }, [creatorId]);
-
-
-    function handleClickDelete(id: string, referenceId: string) {
-        if (referenceId === "daily_transaction") {
-            axios
-                .delete("/api/budget-app/" + id)
-                .then(() => {
-                    setTransactions((prevTransactions) => {
-                        return prevTransactions.filter((transaction) => transaction.id !== id);
-                    });
-                })
-                .catch((error) => {
-                    console.error("Fehler beim Löschen", error);
-                });
-        } else {
-            setDeleteId(id);
-            setDeleteReferenceId(referenceId);
-            setIsDeleteWindowVisible(true);
-        }
-
-
-    }
 
     const sortedTransactions = transactions.slice().sort((a: Transaction, b: Transaction) => {
         const dateA = new Date(a.timeLogged).getTime();
@@ -149,9 +100,7 @@ export default function AllTransactionsPage() {
             <BackButton/>
             <AppMenu activePage="transactions"/>
             <TransactionDetailsWindow transaction={detailsTransaction} isVisible={isDetailsWindowVisible}
-                                      setIsVisible={setIsDetailsWindowVisible}/>
-            <DeleteRecurringWindow setIsVisible={setIsDeleteWindowVisible} isVisible={isDeleteWindowVisible}
-                                   id={deleteId} referenceId={deleteReferenceId} setTransactions={setTransactions}/>
+                                      setIsVisible={setIsDetailsWindowVisible} setTransactions={setTransactions}/>
             <h2>Past transactions:</h2>
             {Object.keys(groupedTransactions).map((dateKey: string) => (
                 <div key={dateKey}>
@@ -166,12 +115,6 @@ export default function AllTransactionsPage() {
                                     <span>{formatMoney(transaction.amountOfMoney.toString())}€</span>
                                 </DataContainer>
                                 <Category>{transaction.transactionCategory}</Category>
-                                <DeleteButton
-                                    type="button"
-                                    onClick={() => handleClickDelete(transaction.id, transaction.referenceId)}
-                                >
-                                    <ButtonImage src={TrashIcon} alt="Trash Icon"/>
-                                </DeleteButton>
                             </ListItem>
                         ))}
                     </List>
